2183833cd7cd2ed895f19c447478ca5426b54f45
Miloš Jozek <90444097+BoldHeadBall@users.noreply.github.com>
2022-02-17 10:32:21 +0100

    Update TeacherPageController.java

--- src/attendance/gui/controller/TeacherPageController.java
+++ src/attendance/gui/controller/TeacherPageController.java
@@ -5,9 +5,10 @@ import attendance.be._Class;
 import attendance.gui.model.TeacherModel;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-import javax.swing.table.TableColumn;
-import javax.swing.text.TableView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,9 +16,10 @@ import java.util.ResourceBundle;
 public class TeacherPageController implements Initializable {
     TeacherModel teacherModel = new TeacherModel();
     MainApp mainApp;
-
     @FXML
-    private TableColumn  teacherClass;
+    private TableView<_Class> classTBV;
+    @FXML
+    private TableColumn<_Class, String> teacherClass;
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
@@ -25,7 +27,8 @@ public class TeacherPageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        this.classTBV.setItems(teacherModel.getAllClasses());
+        this.teacherClass.setCellValueFactory(new PropertyValueFactory<>("name"));
     }
 }
 
